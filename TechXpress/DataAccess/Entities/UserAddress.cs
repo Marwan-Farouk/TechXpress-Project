afa584c848b255f11d0fa3dcb31ef7a62d2a66f6
Omar Refay <omarrefay2004@gmail.com>
2025-03-10 02:54:42 +0200

    repositories pattern Rename Folder

--- TechXpress/DataAccess/Entities/UserAddress.cs
+++ TechXpress/DataAccess/Entities/UserAddress.cs
@@ -16,7 +16,8 @@ namespace DataAccess.Entities
         public string Street { get; set; }
         public string BuildingNumber { get; set; }
         public string ApartmentNumber { get; set; }
-        public ICollection<Order> Orders { get; set; }
+        public ICollection<Order> Orders { get; set; 
+        }
 
     }
 }
