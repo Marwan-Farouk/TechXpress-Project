7bbe4105a4503132eb36dbf35df6f0ce6ec2ece0
Marwan-Farouk <marwanfarouk3284u@gmail.com>
2025-03-12 01:05:11 +0200

    Add FileService class and update project dependencies
    Updated `Business.csproj` to include package references for
    `Microsoft.AspNetCore.Http.Features` version `5.0.17` and
    `Microsoft.Extensions.Features` version `8.0.14`.
    
    Added `FileService` class with `UploadFile` method to handle
    file uploads, generating unique file names and saving files
    to the `wwwroot` directory.

---
+++ TechXpress/Business/Services/FileService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BusinessLayer.Services
+{
+    public class FileService
+    {
+        public string UploadFile(IFormFile file, string destinationFolder)
+        {
+            var uniqueFileName = string.Empty;
+
+            if (file != null && file.Length > 0)
+            {
+                var uploadsFolder = Path.Combine(@"./wwwroot/", destinationFolder);   
+
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;  
+
+                var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+
+            }
+            return uniqueFileName;  
+        }
+    }
+}
