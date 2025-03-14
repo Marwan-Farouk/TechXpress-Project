9ad69bccfb18a38970df9bc0638110719ec0bb53
yousef <youseffarahat61@gmail.com>
2025-03-14 03:01:48 +0200

    Implemented Model, View, and Controller for Category. Added full CRUD operations (Create, Read, Update, Delete).

---
+++ TechXpress/Presentation/Models/Categore.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BestStoreMVC.Models
+{
+    public class Categore
+    {
+        
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public decimal Stock { get; set; }
+
+    }
+}
---
+++ TechXpress/Presentation/Models/CategoreDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BestStoreMVC.Models
+{
+    public class CategoreDto
+    {
+
+        [Required,MaxLength(100)]
+        public string Name { get; set; } = "";
+        [Required]
+        public string Description { get; set; } = "";
+        [Required]
+        public decimal Stock { get; set; }
+    }
+}
