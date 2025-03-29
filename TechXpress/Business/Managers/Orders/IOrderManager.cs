b925b797f0d0885c2f52b8a64c0ff5d4639baab1
yousef <youseffarahat61@gmail.com>
2025-03-29 03:07:08 +0200

    make shopcart views , viewmodel , controller

--- TechXpress/Business/Managers/Orders/IOrderManager.cs
+++ TechXpress/Business/Managers/Orders/IOrderManager.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Business.DTOs.Orders;
+using DataAccess.Entities;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Business.Managers.Orders
 {
     public interface IOrderManager
     {
+        // Order Creation
+        Task CreateOrder(CreateOrderDto orderDto);
+
+        // Order Retrieval
+        Task<Order> GetOrderById(int id);
+        Task<List<Order>> GetAllOrders();
+        Task<List<Order>> GetOrdersByUserId(int userId);
+        Task<List<Order>> GetOrdersByStatus(string status);
+        Task<List<Order>> GetOrdersByUserIdAndStatus(int userId, string status);
+        Task<List<Order>> GetOrdersInDateRange(DateTime startDate, DateTime endDate);
+
+        // Order Management
+        Task UpdateOrder(Order order);
+        Task DeleteOrder(int id);
+
+        // Additional methods from your OrderManager
+        Task SaveChangesAsync();
     }
 }
--- TechXpress/Business/Managers/Orders/OrderManager.cs
+++ TechXpress/Business/Managers/Orders/OrderManager.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Business.DTOs.Orders;
-using Business.Managers.Products;
+﻿using Business.DTOs.Orders;
 using DataAccess.Entities;
 using DataAccess.Repositories.ORDER;
 using DataAccess.Repositories.PRODUCT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Business.Managers.Orders
 {
@@ -15,18 +13,18 @@ namespace Business.Managers.Orders
     {
         private readonly IOrderRepository _orderRepository;
         private readonly IProductRepository _productRepository;
-        
-        public OrderManager(IOrderRepository orderRepository , IProductRepository productRepository)
+
+        public OrderManager(IOrderRepository orderRepository, IProductRepository productRepository)
         {
-            _orderRepository = orderRepository; 
+            _orderRepository = orderRepository;
             _productRepository = productRepository;
         }
 
         public async Task CreateOrder(CreateOrderDto orderDto)
         {
+            // Your existing implementation
             var order = orderDto.ToOrder();
             await _orderRepository.Add(order);
-
             await _orderRepository.SaveChangesAsync();
 
             order.OrderItems = orderDto.ProductIds.Select(id => new OrderDetails
@@ -38,10 +36,33 @@ namespace Business.Managers.Orders
             }).ToList();
 
             await _orderRepository.SaveChangesAsync();
-
         }
 
+        public async Task<Order> GetOrderById(int id)
+            => await _orderRepository.GetById(id);
 
+        public async Task<List<Order>> GetAllOrders()
+            => await _orderRepository.GetAll();
 
+        public async Task<List<Order>> GetOrdersByUserId(int userId)
+            => await _orderRepository.GetOrdersByUserId(userId);
+
+        public async Task<List<Order>> GetOrdersByStatus(string status)
+            => await _orderRepository.GetOrdersByStatus(status);
+
+        public async Task<List<Order>> GetOrdersByUserIdAndStatus(int userId, string status)
+            => await _orderRepository.GetOrdersByUserIdAndStatus(userId, status);
+
+        public async Task<List<Order>> GetOrdersInDateRange(DateTime startDate, DateTime endDate)
+            => await _orderRepository.GetOrdersInDateRange(startDate, endDate);
+
+        public async Task UpdateOrder(Order order)
+            => await _orderRepository.Update(order);
+
+        public async Task DeleteOrder(int id)
+            => await _orderRepository.Delete(id);
+
+        public async Task SaveChangesAsync()
+            => await _orderRepository.SaveChangesAsync();
     }
 }
