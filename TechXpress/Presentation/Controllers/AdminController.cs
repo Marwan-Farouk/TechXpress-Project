5422509113b067ea9c4bba7c89f20841bbe6ef87
Marwan-Farouk <marwanfarouk3284u@gmail.com>
2025-04-28 04:08:55 +0300

    Add role management functionality
    Introduced `RoleController` with role listing and creation actions. Added corresponding views (`Index` and `Create`) alongside a `CreateRoleActionRequest` model for role creation. Enhanced `AdminController` by adding an action to assign user roles, preparing for future role management features.

--- TechXpress/Presentation/Controllers/AdminController.cs
+++ TechXpress/Presentation/Controllers/AdminController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Diagnostics;
 
 using Business.DTOs.Products;
-
+using DataAccess.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Presentation.Models;
 
 namespace Presentation.Controllers;
 
 public class AdminController : Controller
 {
+    private readonly UserManager<User> _userManager;
+    private readonly RoleManager<Role> _roleManager;
 
+    public AdminController(UserManager<User> userManager,RoleManager<Role> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
 
     public IActionResult Index()
     {
@@ -18,7 +27,18 @@ public class AdminController : Controller
         return View(products);
     }
 
-
+    [HttpGet]
+    public async Task<IActionResult> AssignUserRole()
+    {
+        var users = await _userManager.Users.ToListAsync();
+        var roles = await _roleManager.Roles.ToListAsync();
+        return View();
+    }
+    // [HttpPost]
+    // public IActionResult AssignUserRole()
+    // {
+    //     return View();
+    // }
 
 
 }
---
+++ TechXpress/Presentation/Controllers/RoleController.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Presentation.ActionRequests;
+
+namespace Presentation.Controllers;
+
+public class RoleController : Controller
+{
+    private readonly RoleManager<Role> _roleManager;
+
+    public RoleController(RoleManager<Role> roleManager)
+    {
+        _roleManager = roleManager;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var roles = await _roleManager.Roles.ToListAsync();
+        return View(roles);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateRoleActionRequest request)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _roleManager.CreateAsync(new Role
+            {
+                Name = request.Name
+            });
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+            }
+        }
+        return View(request);
+    }
+}
